5e92785eb18874758de818141fb0c5949277c619
zahid-official <zahid.official8@gmail.com>
2026-04-09 02:08:43 +0600

    feat: implement newsletter section in home page

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,5 +1,6 @@
 import AboutSection from "@/components/modules/public/home/AboutSection";
 import BannerSection from "@/components/modules/public/home/BannerSection";
+import NewsletterSection from "@/components/modules/public/home/NewsletterSection";
 import ProjectSection from "@/components/modules/public/home/ProjectSection";
 import ServiceSection from "@/components/modules/public/home/ServiceSection";
 
@@ -11,6 +12,7 @@ const HomePage = () => {
       <AboutSection />
       <ServiceSection />
       <ProjectSection />
+      <NewsletterSection />
     </div>
   );
 };
