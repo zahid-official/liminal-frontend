4373478854b600b66f3c0fd6ecc50c48d66ec53c
zahid-official <zahid.official8@gmail.com>
2026-04-09 03:30:14 +0600

    feat: implement faq section in homepage

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,6 +1,7 @@
 import AboutSection from "@/components/modules/public/home/AboutSection";
 import BannerSection from "@/components/modules/public/home/BannerSection";
 import BlogSection from "@/components/modules/public/home/BlogSection";
+import FaqSection from "@/components/modules/public/home/FaqSection";
 import NewsletterSection from "@/components/modules/public/home/NewsletterSection";
 import ProjectSection from "@/components/modules/public/home/ProjectSection";
 import ServiceSection from "@/components/modules/public/home/ServiceSection";
@@ -13,6 +14,7 @@ const HomePage = () => {
       <AboutSection />
       <ServiceSection />
       <ProjectSection />
+      <FaqSection />
       <BlogSection />
       <NewsletterSection />
     </div>
