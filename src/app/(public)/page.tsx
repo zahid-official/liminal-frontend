b605c451c4a48bdb8e8b3bd38a3a58fbe607c381
zahid-official <zahid.official8@gmail.com>
2026-04-11 17:52:22 +0600

    feat: implement experience section in homepage

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,6 +1,7 @@
 import AboutSection from "@/components/modules/public/home/AboutSection";
 import BannerSection from "@/components/modules/public/home/BannerSection";
 import BlogSection from "@/components/modules/public/home/BlogSection";
+import ExperienceSection from "@/components/modules/public/home/ExperienceSection";
 import FaqSection from "@/components/modules/public/home/FaqSection";
 import NewsletterSection from "@/components/modules/public/home/NewsletterSection";
 import ProjectSection from "@/components/modules/public/home/ProjectSection";
@@ -13,6 +14,7 @@ const HomePage = () => {
     <div>
       <BannerSection />
       <AboutSection />
+      <ExperienceSection />
       <ServiceSection />
       <ProjectSection />
       <QuoteSection />
