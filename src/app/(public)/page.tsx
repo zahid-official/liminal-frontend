c91756798034c1085a9e8296a1fcc70cf698f5e5
zahid-official <zahid.official8@gmail.com>
2026-04-30 23:10:06 +0600

    fix: update projects page

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,13 +1,3 @@
-import AboutSection from "@/components/modules/public/home/AboutSection";
-import BannerSection from "@/components/modules/public/home/BannerSection";
-import BlogSection from "@/components/modules/public/home/BlogSection";
-import ExperienceSection from "@/components/modules/public/home/ExperienceSection";
-import FaqSection from "@/components/modules/public/home/FaqSection";
-import ProjectSection from "@/components/modules/public/home/ProjectSection";
-import NewsletterSection from "@/components/modules/public/home/NewsletterSection";
-import QuoteSection from "@/components/modules/public/home/QuoteSection";
-import ServiceSection from "@/components/modules/public/home/ServiceSection";
-import TestimonialSection from "@/components/modules/public/home/TestimonialSection";
 import { Metadata } from "next";
 
 // Metadata for the Home Page
@@ -19,20 +9,7 @@ export const metadata: Metadata = {
 
 // HomePage Component
 const HomePage = () => {
-  return (
-    <div>
-      <BannerSection />
-      <AboutSection />
-      <ExperienceSection />
-      <ProjectSection />
-      <ServiceSection />
-      <TestimonialSection />
-      <QuoteSection />
-      <FaqSection />
-      <BlogSection />
-      <NewsletterSection />
-    </div>
-  );
+  return <div></div>;
 };
 
 export default HomePage;
--- src/app/(public)/projects/page.tsx
+++ src/app/(public)/projects/page.tsx
@@ -1,16 +1,21 @@
-import ProjectList from "@/components/modules/public/projects/ProjectList";
 import PageHeader from "@/components/shared/PageHeader";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Our Projects | Liminal",
+  description:
+    "Explore our portfolio of premium interior design and architectural projects. From residential villas to commercial spaces, we bring creative visions to life.",
+};
 
 // ProjectsPage Component
 const ProjectsPage = () => {
   return (
     <main>
       <PageHeader
-        title="Our Portfolio"
+        title="Our Projects"
         items={[{ label: "Home", href: "/" }, { label: "Projects" }]}
-        bgImage="/assets/bg-project.webp"
+        bgImage="/assets/projects/bg-project-premium.png"
       />
-      <ProjectList />
     </main>
   );
 };
