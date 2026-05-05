4cab93ef003aada5d6dd258f34b61827378c2a84
zahid-official <zahid.official8@gmail.com>
2026-05-05 23:26:37 +0600

    feat: implement about us page

---
+++ src/app/(public)/about-us/page.tsx
@@ -0,0 +1,24 @@
+import PageHeader from "@/components/shared/PageHeader";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "About Us | Liminal",
+  description:
+    "Discover the philosophy, story, and people behind Liminal. We are a premium interior design studio dedicated to creating evocative, functional, and timeless spaces.",
+};
+
+// AboutUsPage Component
+const AboutUsPage = () => {
+  return (
+    <main>
+      {/* Hero Section */}
+      <PageHeader
+        title="About Liminal"
+        items={[{ label: "Home", href: "/" }, { label: "About Us" }]}
+        bgImage="/assets/about-us/bg-about.png"
+      />
+    </main>
+  );
+};
+
+export default AboutUsPage;
